07d48335e539bab460b5ae012a0e8b64c393daef
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-05 19:01:27 -0300

    feat: Finalizando o CRUD de Pets

--- src/routes/petRouter.ts
+++ src/routes/petRouter.ts
@@ -6,5 +6,8 @@ const router = express.Router();
 const petController = new PetController();
 
 router.post('/', (req, res) => {petController.criaPet(req, res)})
+router.get('/', (req, res) => {petController.listaPets(req, res)})
+router.put('/:id', (req, res) => {petController.atualizaPet(req, res)})
+router.delete('/:id', (req, res) => {petController.deletaPet(req, res)})
 
 export default router
