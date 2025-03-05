cc202352c8fcf2c4e5848f5026e2b5cd520e13bf
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-05 18:42:55 -0300

    feat: Criando o tipo personalizado TypePet

--- src/controllers/PetController.ts
+++ src/controllers/PetController.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
+import type TypePet from "../types/TypePet";
 
-let listaDePets = [];
+let listaDePets: TypePet[] = [];
+// let listaDePets: Array<TypePet> = [];
 
 export default class PetController {
 
     criaPet(req:Request, res:Response) {
-        const novoPet = req.body;
+        // const { id, adotado, especie, idade, nome } = <TypePet> req.body;
+        const { id, adotado, especie, idade, nome } = req.body as TypePet;
+
+        const novoPet: TypePet = { id, adotado, especie, idade, nome }
 
         listaDePets.push(novoPet);
         
---
+++ src/types/TypePet.ts
@@ -0,0 +1,9 @@
+type TypePet = {
+    id: number
+    nome: string
+    especie: string
+    adotado: boolean
+    idade: number
+}
+
+export default TypePet
