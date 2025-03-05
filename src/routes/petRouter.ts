ac41a0ec1f16899c611c7eadfd815b9c1279a288
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-05 16:31:01 -0300

    feat: Criando o controller e a rota para cadastrar um pet

---
+++ src/routes/petRouter.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import PetController from "../controllers/PetController";
+
+const router = express.Router();
+
+const petController = new PetController();
+
+router.post('/', (req, res) => {petController.criaPet(req, res)})
+
+export default router
