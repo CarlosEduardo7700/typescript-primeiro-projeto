d0aeb9da605048b161a86562528c6fa965aab5cf
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-07 20:42:20 -0300

    feat: Finalizando o CRUD de Adotante

--- src/repositories/AdotanteRepository.ts
+++ src/repositories/AdotanteRepository.ts
@@ -13,6 +13,68 @@ export default class AdotanteRepository implements IAdotanteRepository {
         await this.repository.save(adotante)
     }
 
+    async listaAdotantes(): Promise<AdotanteEntity[]> {
+        return await this.repository.find()
+    }
+
+    async atualizaAdotante(
+        id: number, 
+        adotante: AdotanteEntity
+    ): Promise<{ success: boolean; message?: string; }> {
+        try {
+            const adotanteToUpdate = await this.repository.findOne({
+                where: {
+                    id
+                }
+            })
+
+            if (!adotanteToUpdate) {
+                return { success: false, message: "Adotante não encontrado!" }
+            }
+
+            Object.assign(adotanteToUpdate, adotante);
+
+            await this.repository.save(adotanteToUpdate);
+
+            return { success: true };
+            
+        } catch (error) {
+            console.log(error);
+
+            return {
+                success: false,
+                message: "Ocorreu um erro ao tentar atualizar o adotante.",
+            };
+        }
+    }
+
+    async deletaAdotante(
+        id: number
+    ): Promise<{ success: boolean; message?: string }> {
+        try {
+            const adotanteToRemove = await this.repository.findOne({ 
+                where: { 
+                    id 
+                } 
+            });
+
+            if (!adotanteToRemove) {
+            return { success: false, message: "Adotante não encontrado!" };
+            }
+
+            await this.repository.remove(adotanteToRemove);
+
+            return { success: true };
+        } catch (error) {
+
+            return {
+                success: false,
+                message: "Ocorreu um erro ao tentar excluir o adotante.",
+            };
+        }
+    }
+        
+
 
 
 }
--- src/repositories/interfaces/IAdotanteRepository.ts
+++ src/repositories/interfaces/IAdotanteRepository.ts
@@ -2,4 +2,15 @@ import AdotanteEntity from "../../entities/AdotanteEntity";
 
 export default interface IAdotanteRepository {
     criaAdotante(adotante: AdotanteEntity): void | Promise<void>
+
+    listaAdotantes(): AdotanteEntity[] | Promise<AdotanteEntity[]>
+
+    atualizaAdotante(
+        id: number,
+        adotante: AdotanteEntity
+    ): Promise<{ success: boolean; message?: string }> | void
+
+    deletaAdotante(
+        id: number
+    ): Promise<{ success: boolean; message?: string }> | void
 }
