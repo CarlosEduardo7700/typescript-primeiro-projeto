ae561d31ae09602a51227599705995a1c277d494
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-10 20:40:59 -0300

    fail: Criando um middleware para tratar os erros da aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
-import express, { Response } from "express";
+import "express-async-errors";
+import express from "express";
 import router from "./routes";
 import "reflect-metadata"
 import { AppDataSource } from "./config/dataSource";
+import { erroMiddleware } from "./middleware/erro";
 
 const app = express();
 app.use(express.json());
 router(app)
 
+// app.get('/test', () => {
+//   throw new Error("teste de erro")
+// })
+
+app.use(erroMiddleware);
+
 AppDataSource.initialize()
   .then(() => console.log("Banco de Dados conectado!"))
   .catch((erro) => console.log(erro))
+
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import "express-async-errors";
 import express from "express";
 import petRouter from "../routes/petRouter"
 import adotanteRouter from "../routes/adotanteRouter"
